f8c050bb024489600b62ba2f37f6bb9477dc6dec
Loc Mai <lmai@axon.com>
2021-09-11 01:23:45 +0700

    finish sample in python, add k8s + skaffold

--- apps_pull/python/main.py
+++ apps_pull/python/main.py
@@ -1,9 +1,7 @@
-from prometheus_client import make_wsgi_app, Summary
-from flask import Flask
+from prometheus_client import make_wsgi_app, Summary, Counter
+from flask import Flask, abort
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-
 import random
-import time
 
 app = Flask(__name__)
 
@@ -11,12 +9,23 @@ app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
     '/metrics': make_wsgi_app()
 })
 
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+s = Summary('request_processing_seconds', 'Time spent processing request', ['path'])
+c = Counter('my_requests', 'Total requests received', ['path','method', 'status'])
 
-@REQUEST_TIME.time()
+@s.labels('/').time()
 @app.route("/")
 def index():
-    return "<p>OK!</p>"
+    c.labels('/','GET','200').inc()
+    return "<p>OK - path: /</p>"
+
+@s.labels('/random').time()
+@app.route('/random')
+def randomly_fail():
+    if random.random() < 0.5:
+        c.labels('/random','GET','503').inc()
+        abort(503)
+    c.labels('/random','GET','200').inc()
+    return "<p>OK - path: /random</p>"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=8080,threaded=True)
